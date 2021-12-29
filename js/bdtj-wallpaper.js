c4dc4bedc8f52169db7370bc3140aa527b1d30fa
Beard Uncle <73875469+bearduncle520@users.noreply.github.com>
2021-12-29 10:29:24 +0800

    Create bdtj-wallpaper.js

---
+++ js/bdtj-wallpaper.js
@@ -0,0 +1,8 @@
+/*ZHUYE.XYZ*/
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?0a7c6718dbd208af041aaa9a17c0954f";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
