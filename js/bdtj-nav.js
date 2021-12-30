0bc3e9897b2c75b3e7b239b307618535e5b2de1a
Beard Uncle <73875469+bearduncle520@users.noreply.github.com>
2021-12-30 11:22:32 +0800

    Update bdtj-nav.js

--- js/bdtj-nav.js
+++ js/bdtj-nav.js
@@ -6,12 +6,3 @@ var _hmt = _hmt || [];
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
-
-/*ZHUYE.RED*/
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?d7f514bd13fcbd1cef649b5d4d6671f2";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
