cf795a541fa6fdc0693c1c1d202c9ab4484c86c9
Beard Uncle <73875469+bearduncle520@users.noreply.github.com>
2021-12-30 11:22:00 +0800

    Update bdtj-movie.js

--- js/bdtj-movie.js
+++ js/bdtj-movie.js
@@ -6,12 +6,3 @@ var _hmt = _hmt || [];
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
-
-/*ZHUYE.RED*/
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?f3401c8aeb45bf769d0c878c90e09cf4";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
