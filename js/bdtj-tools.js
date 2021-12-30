84407f1de1cbaed5685dd461d6d3d84a7734b31b
Beard Uncle <73875469+bearduncle520@users.noreply.github.com>
2021-12-30 11:21:03 +0800

    Create bdtj-tools.js

---
+++ js/bdtj-tools.js
@@ -0,0 +1,8 @@
+/*ZHUYE.XYZ*/
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?c407f815718b257459ec887047596915";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
