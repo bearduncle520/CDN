847c9a836e8df17dcbda3ddafc972c987f6f41d8
Beard Uncle <73875469+bearduncle520@users.noreply.github.com>
2021-12-30 11:21:39 +0800

    Update bdtj-game.js

--- js/bdtj-game.js
+++ js/bdtj-game.js
@@ -6,12 +6,3 @@ var _hmt = _hmt || [];
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
-
-/*ZHUYE.RED*/
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?6dd41bc2c7f43af58634f5a97a860c4c";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
