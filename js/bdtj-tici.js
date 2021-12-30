d68614b142c035bb01358858dcaa1a3944e2f068
Beard Uncle <73875469+bearduncle520@users.noreply.github.com>
2021-12-30 11:24:25 +0800

    Update bdtj-tici.js

--- js/bdtj-tici.js
+++ js/bdtj-tici.js
@@ -6,12 +6,3 @@ var _hmt = _hmt || [];
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
-
-/*TICI.XYZ*/
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?c452a9f0ef15880bca8bafafb6c15123";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
