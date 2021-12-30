ecfdc5b2a52c5066895ca773251584d00601c3d2
Beard Uncle <73875469+bearduncle520@users.noreply.github.com>
2021-12-30 11:22:47 +0800

    Update bdtj-noize.js

--- js/bdtj-noize.js
+++ js/bdtj-noize.js
@@ -6,12 +6,3 @@ var _hmt = _hmt || [];
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
-
-/*ZHUYE.RED*/
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?f3c8b8b997ede56663cc6fc7502e7861";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
